2bb79dccf70d25ce86cdc38f35ce06687da8aeb9
erik-svensson97 <bibbebo@hotmail.com>
2022-08-08 19:47:24 +0200

    dd
    dd

--- src/p1/MessageManager.java
+++ src/p1/MessageManager.java
@@ -1,9 +1,11 @@
 package p1;
 
+import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeSupport;
 
-public class MessageManager {
+public class MessageManager{
+    private Thread thread;
     Buffer<Message> messageBuffer;
     private final PropertyChangeSupport changes = new PropertyChangeSupport(this);
 
@@ -16,6 +18,45 @@ public class MessageManager {
     public void addPropertyChangeListener( PropertyChangeListener listener ){
         changes.addPropertyChangeListener(listener);
     }
+
+    public void start(){
+        thread= new WindowUpdater(messageBuffer);
+        thread.start();
+
+    }
+
+
+
+    private class WindowUpdater extends Thread{
+        Buffer<Message> messageBuffer;
+        Message message;
+
+        public WindowUpdater(Buffer<Message> messageBuffer) {
+            this.messageBuffer=messageBuffer;
+        }
+
+        @Override
+        public void run() {
+            while(thread!=null){
+                try {
+                    Thread.sleep(1000);
+                    message=messageBuffer.get();
+                    changes.firePropertyChange("message", null,  message);
+
+
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        }
+    }
+
+
+
+
+
+
 
 
 
--- src/p1/P1Viewer.java
+++ src/p1/P1Viewer.java
@@ -1,11 +1,32 @@
 package p1;
 
-public class P1Viewer extends Viewer{
+import javax.swing.*;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
+public class P1Viewer extends Viewer  {
     private MessageManager messageManager;
 
     public P1Viewer(MessageManager messageManager,int width, int height) {
         super(width, height);
         this.messageManager=messageManager;
+        messageManager.addPropertyChangeListener((PropertyChangeListener) this);
 
+    }
+
+    private class WindowUpdater implements PropertyChangeListener{
+        @Override
+        public void propertyChange(PropertyChangeEvent evt) {
+            SwingUtilities.invokeLater(new Runnable() {
+                public void run() {
+                    if(evt.getPropertyName().equals("game") &&
+                            evt.getNewValue() instanceof Message) {
+
+                        Message message = (Message) evt.getNewValue();
+
+                    }
+                }
+            });
+        }
     }
 }
